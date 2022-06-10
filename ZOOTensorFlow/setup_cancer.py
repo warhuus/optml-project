79e2ddad7c7a2a283ab4f3d19dd09bf486845b61
dtubfg <43341816+dtubfg@users.noreply.github.com>
2022-06-10 15:33:41 +0200

    Move some training images to test file

--- ZOOTensorFlow/setup_cancer.py
+++ ZOOTensorFlow/setup_cancer.py
@@ -21,7 +21,7 @@ data_transforms_test = albumentations.Compose([
 ])
 
 def get_labels(base_dir):
-    files = os.listdir(os.path.join(base_dir, "train"))
+    files = os.listdir(os.path.join(base_dir, "train")) + os.listdir(os.path.join(base_dir, "test"))
     files_noext = [i.split(".")[0] for i in files]
     labels = pd.read_csv(os.path.join(base_dir, 'train_labels.csv'))
     return labels.loc[labels["id"].isin(files_noext)]
@@ -31,33 +31,46 @@ def get_img_class_dict(base_dir):
     img_class_dict = {k:v for k, v in zip(labels.id, labels.label)}
     return img_class_dict
 
+def load_images(datapath, img_class_dict):
+    data = []
+    labels = []
+
+    for f in os.listdir(datapath):
+        img_name = os.path.join(datapath, f)
+        img = cv2.imread(img_name)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        image = data_transforms_test(image=img)
+        image = image['image']
+
+        img_name_short = f.split('.')[0]
+        label = img_class_dict[img_name_short]
+
+        data += [image.numpy()]
+        labels += [np.array(label)]
+
+    return data, labels
+
 
 class CancerDataset:
-    def __init__(self, datapath = os.path.join('Cancer', 'input', 'train')):
-        train_data = []
-        train_labels = []
+    def __init__(self):
+        
+        datapath = os.path.join('Cancer', 'input', 'train')
+        test_datapath = os.path.join('Cancer', 'input', 'test')
 
         if not os.path.exists(datapath):
             raise FileNotFoundError()
 
         img_class_dict = get_img_class_dict(os.path.join('Cancer', 'input'))
-        for f in os.listdir(datapath):
-            img_name = os.path.join(datapath, f)
-            img = cv2.imread(img_name)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            image = data_transforms_test(image=img)
-            image = image['image']
 
-            img_name_short = f.split('.')[0]
-            label = img_class_dict[img_name_short]
+        data, labels = load_images(datapath, img_class_dict)
+        train_data = np.array(data, dtype=np.float32)
+        train_labels = np.array(labels)
 
-            train_data += [image.numpy()]
-            train_labels += [np.array(label)]
-            
-        train_data = np.array(train_data, dtype=np.float32)
-        train_labels = np.array(train_labels)
-        
-        VALIDATION_SIZE = 150
+        data, labels = load_images(test_datapath, img_class_dict)
+        self.test_data = np.array(data, dtype=np.float32)
+        self.test_labels = np.array(labels)
+
+        VALIDATION_SIZE = 100
         
         self.validation_data = train_data[:VALIDATION_SIZE, :, :, :]
         self.validation_labels = train_labels[:VALIDATION_SIZE]
