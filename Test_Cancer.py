7137bc0d65ecee38254bfeeb7db60814de0fb6dd
DeterjoSimon <simon-dk@otmail.fr>
2022-06-15 16:28:13 +0200

    Changed the Test_Cancer script to be compatible with LSF cluster. Changed requirements to also be compatible with it.

--- Test_Cancer.py
+++ Test_Cancer.py
@@ -19,24 +19,27 @@ data, target = next(iter(dataloader))
 
 class LossCancer(object):
 
-    def __init__(self, model, img, img_shape, true_lbl) -> None:
-        self.model = model
-        self.true_img = img
-        self.true_lbl = true_lbl
-        self.lambda_ = 0.9
-        self.shape = img_shape
-    
-    def __call__(self, delta):
-        input = torch.reshape(self.true_img + delta, self.shape)
-        with torch.no_grad():
-            output = self.model(input.float())
-        pr = output[:, 0][0].numpy()
-        fx = sigmoid(pr)
-        return ((fx - 1 - self.true_lbl) ** 2 +
-                self.lambda_ * np.linalg.norm(delta, 0))
+	def __init__(self, model, img, img_shape, true_lbl, device) -> None:
+		self.device = device
+		self.model = model
+		self.true_img = img.to(self.device)
+		self.true_lbl = true_lbl
+		self.lambda_ = 0.9
+		self.shape = img_shape
+	
+	def __call__(self, delta):
+		input = torch.reshape(self.true_img + torch.tensor(delta).to(self.device), self.shape)
+		with torch.no_grad():
+			output = self.model(input.type(torch.cuda.FloatTensor))
+		pr = output[:, 0][0].cpu().numpy()
+		fx = sigmoid(pr)
+		return ((fx - 1 - self.true_lbl) ** 2 +
+				self.lambda_ * np.linalg.norm(delta, 0))
+		  
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-model = get_model('Cancer', 'cpu')
-obj_func = LossCancer(model=model,  img = data.flatten(), true_lbl= target, img_shape=data.shape)
+model = get_model('Cancer', device)
+obj_func = LossCancer(model=model,  img = data.flatten(), true_lbl= target, img_shape=data.shape, device=device)
 
 n = 2000
 s = int(0.1*n)
@@ -51,8 +54,8 @@ sparsity = s
 
 # Parameters for ZORO. Defaults are fine in most cases
 params = {"step_size":1.0, "delta": 0.0001, "max_cosamp_iter": 10, 
-          "cosamp_tol": 0.5,"sparsity": sparsity,
-          "num_samples": int(np.ceil(np.log(len(x0))*sparsity))}
+		  "cosamp_tol": 0.5,"sparsity": sparsity,
+		  "num_samples": int(np.ceil(np.log(len(x0))*sparsity))}
 
 performance_log_ZORO = [[0, obj_func(x0)]]
 
@@ -62,22 +65,23 @@ opt  = ZORO(x0, obj_func, params, function_budget= int(1e6))
 
 # the actual optimization routine
 termination = False
+print("Here")
 while termination is False:
-    # optimization step
-    # solution_ZORO = False until a termination criterion is met, in which 
-    # case solution_ZORO = the solution found.
-    # termination = False until a termination criterion is met.
-    # If ZORO terminates because function evaluation budget is met, 
-    # termination = B
-    # If ZORO terminated because the target accuracy is met,
-    # termination= T.
-    print('yo')
-    evals_ZORO, solution_ZORO, termination = opt.step()
+	# optimization step
+	# solution_ZORO = False until a termination criterion is met, in which 
+	# case solution_ZORO = the solution found.
+	# termination = False until a termination criterion is met.
+	# If ZORO terminates because function evaluation budget is met, 
+	# termination = B
+	# If ZORO terminated because the target accuracy is met,
+	# termination= T.
+	print('yo')
+	evals_ZORO, solution_ZORO, termination = opt.step()
 
-    # save some useful values
-    performance_log_ZORO.append( [evals_ZORO,np.mean(opt.fd)] )
-    # print some useful values
-    opt.report( 'Estimated f(x_k): %f  function evals: %d\n' %
-        (np.mean(opt.fd), evals_ZORO) )
+	# save some useful values
+	performance_log_ZORO.append( [evals_ZORO,np.mean(opt.fd)] )
+	# print some useful values
+	opt.report( 'Estimated f(x_k): %f  function evals: %d\n' %
+		(np.mean(opt.fd), evals_ZORO) )
 
 # for idx, (data, target) in enumerate(iter(dataloader)):
