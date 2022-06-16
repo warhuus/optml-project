1d68bc1bba40be02bfb565518dd7ce1ff974dd56
dtubfg <43341816+dtubfg@users.noreply.github.com>
2022-06-16 10:29:38 +0200

    add map_location argument to model load

--- FashionMNIST/utils.py
+++ FashionMNIST/utils.py
@@ -40,6 +40,6 @@ class FashionCNN(nn.Module):
 
 def get_model(path: str, device) -> nn.Module:
     model = FashionCNN()
-    model.load_state_dict(torch.load(path))
+    model.load_state_dict(torch.load(path), map_location=device)
     model.eval()
     return model.to(device)
